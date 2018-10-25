fcf9254a8334fb60eccec039ba1390b135b28922
Meni Besso <meni.b@quali.com>
2018-10-25 15:35:57 +0300

    update

--- applications/apache/apache-init.sh
+++ applications/apache/apache-init.sh
@@ -20,3 +20,4 @@ systemctl enable apache2
 
 echo "Configuring the listening port"
 sed -i "s/Listen 80/Listen $port_number/g" /etc/apache2/ports.conf
+sed -i "s/VirtualHost *:80/VirtualHost *:$port_number /g" /etc/apache2/sites-enabled/000-default.conf
