0046357c3b8b5ccf3a11947d2ef00b9b7e695f29
Meni Besso <meni.b@quali.com>
2018-10-25 15:12:24 +0300

    update

--- applications/apache/apache-init.sh
+++ applications/apache/apache-init.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-# Stop Script on Error
-set -e
+
+set -e      # Stop Script on Error
+
 
 # For Debugging (print env. variables into a file)
 printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
 
 
-# Update packages
-echo "** Updating System **"
+
+echo "Updating System"
 apt-get update -y
 
-echo "** Installing Apache **"
+
+echo "Installing Apache"
 apt-get install apache2 apache2-utils -y
 systemctl enable apache2
 
+
+echo "Configuring the listening port"
+sed -i "s/Listen 80/Listen $port_number/g" /etc/apache2/ports.conf
