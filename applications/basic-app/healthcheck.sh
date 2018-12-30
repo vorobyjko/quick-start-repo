6a83541140d51a1a675d64d53234117e63643f16
Meni Besso <meni.b@quali.com>
2018-12-30 12:05:29 +0200

    update

---
+++ applications/basic-app/healthcheck.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# No need for a real healthcheck because the basic-app is a clean Ubuntu.
+# In case of a failure provisioning the instance, the cloud provider will report an error.
