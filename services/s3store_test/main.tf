6497919ed44558bb985942425c364f35d5915056
vorobyjko <kostiantyn.bogapov@gmail.com>
2019-07-23 17:34:33 +0300

    Delete main.tf

--- services/s3store_test/main.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {}
-resource "aws_s3_bucket_object" "object" {
-  bucket  = "${var.bucket}"
-  key     = "${var.SANDBOX_ID}.json"
-  content = "[\"${var.SANDBOX_DNS}\", \"${var.item1}\", \"${var.item2}\", \"${var.item3}\"]"
-}
